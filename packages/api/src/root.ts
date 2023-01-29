00e741abb6f99c927e65ff2033950bbc271fb617
cristianrdu <raducristiandumitrana@gmail.com>
2023-01-30 00:44:47 +0200

    add routes

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,9 +1,13 @@
 import { createTRPCRouter } from "./trpc";
 import { postRouter } from "./router/post";
 import { authRouter } from "./router/auth";
+import { locationRouter } from "./router/location";
+import { routeRouter } from "./router/route";
 
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  location: locationRouter,
+  route: routeRouter,
   auth: authRouter,
 });
 
