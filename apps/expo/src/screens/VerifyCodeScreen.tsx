431c94fd8547ec8ef5316b560aa2d8d94b7d745d
cristianrdu <raducristiandumitrana@gmail.com>
2023-02-22 18:34:33 +0200

    update verifyCode

--- apps/expo/src/screens/VerifyCodeScreen.tsx
+++ apps/expo/src/screens/VerifyCodeScreen.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import { useClerk, useSignUp } from "@clerk/clerk-expo";
+import { useSignUp } from "@clerk/clerk-expo";
 import { RootStackScreenProps } from "../../types";
-import { styles } from "../components/Styles";
-// import {log} from "../logger";
 
 export default function VerifyCodeScreen({
   navigation,
@@ -24,24 +22,27 @@ export default function VerifyCodeScreen({
 
       await setSession(completeSignUp.createdSessionId);
     } catch (err: any) {
-      // log("Error:> " + err?.status || "");
-      // log("Error:> " + err?.errors ? JSON.stringify(err.errors) : err);
+      console.log("Error:> " + err?.status || "");
+      console.log("Error:> " + err?.errors ? JSON.stringify(err.errors) : err);
     }
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.inputView}>
+    <View className="flex-[1] items-center justify-start bg-[#fff] pt-5">
+      <View className="mb-5 h-12 w-11/12 rounded-md border border-solid border-black">
         <TextInput
           value={code}
-          style={styles.textInput}
+          className="ml-3 h-14 flex-[1] p-3"
           placeholder="Code..."
           placeholderTextColor="#000"
           onChangeText={(code) => setCode(code)}
         />
       </View>
-      <TouchableOpacity style={styles.primaryButton} onPress={onPress}>
-        <Text style={styles.primaryButtonText}>Verify Email</Text>
+      <TouchableOpacity
+        className="mt-12 h-14 w-11/12 items-center justify-center rounded-md bg-black text-white"
+        onPress={onPress}
+      >
+        <Text className="font-bold text-white">Verify Email</Text>
       </TouchableOpacity>
     </View>
   );
